096de8ab9b77e8b7568230187ac0ff53861e370a
tarena <tarena@CrazyMan.local>
2015-07-19 15:37:22 +0800

    init all changes

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by tarena on 15/7/19.
+//  Copyright (c) 2015年 zxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
